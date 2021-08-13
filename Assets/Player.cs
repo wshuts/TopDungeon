4a02a2991149f81c2c264a06b8f6e3070813b164
wshuts <shutswilliam@gmail.com>
2021-08-13 02:02:10 -0500

    logging keyboard input to unity console

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,23 @@ namespace Assets
 {
     public class Player : MonoBehaviour
     {
+        private BoxCollider2D boxCollider;
+        private Vector3 moveDelta;
 
+        private void Start()
+        {
+            boxCollider = GetComponent<BoxCollider2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            moveDelta = Vector3.zero;
+
+            var x = Input.GetAxisRaw("Horizontal");
+            var y = Input.GetAxisRaw("Vertical");
+
+            Debug.Log(x);
+            Debug.Log(y);
+        }
     }
 }
